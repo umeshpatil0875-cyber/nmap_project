38ed482a8ea1aec6731bbbe69fdd7ed55b94be1f
umeshpatil0875-cyber <umeshpatil0875@gmail.com>
2026-04-13 09:52:08 +0530

    Add scanner.py

---
+++ scanner.py
@@ -0,0 +1,13 @@
+from scan_engine.engine import Scanner, ScannerError, scan_target
+from scan_engine.profiles import AGGRESSIVE_PROFILE, BASIC_PROFILE, BANNER_PROFILE, DEEP_PROFILE, SCRIPT_PROFILE
+
+__all__ = [
+    "Scanner",
+    "ScannerError",
+    "scan_target",
+    "BASIC_PROFILE",
+    "DEEP_PROFILE",
+    "AGGRESSIVE_PROFILE",
+    "SCRIPT_PROFILE",
+    "BANNER_PROFILE",
+]
